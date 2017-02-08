47ed8c84386b12123f9248558b3c0fe32b31dc30
hongiaher <hongiaher@gmail.com>
2017-02-07 19:08:21 -0500

    Chapter 11
    Changes made through Ch. 11

--- src/test/java/pageobjects/Base.java
+++ src/test/java/pageobjects/Base.java
@@ -3,6 +3,9 @@ package test.java.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 
@@ -39,4 +42,21 @@ public class Base {
 			return false;
 		}
 	}
+	
+	public Boolean waitForIsDisplayed(By locator, Integer... timeout) {
+		try {
+			waitFor(ExpectedConditions.visibilityOfElementLocated(locator),
+					(timeout.length > 0 ? timeout[0] : null));
+		} catch (org.openqa.selenium.TimeoutException exception) {
+			return false;
+		}
+		return true;
+	}
+
+	private void waitFor(ExpectedCondition<WebElement> condition, Integer timeout) {
+		timeout = timeout != null ? timeout : 5;
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		wait.until(condition);
+	}
+
 }
